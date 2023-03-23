0066b5e46e8ff0892660a0054f8727929d51df5d
Güven Karabulut <guven.07.568@gmail.com>
2023-03-23 23:53:57 +0300

    .

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/controller/UserController.java
@@ -1,17 +1,29 @@
 package com.appsdeveloperblog.app.ws.mobileappws.ui.controller;
 
+import com.appsdeveloperblog.app.ws.mobileappws.ui.model.response.UserRest;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="users")// ! http://localhost:8080/users
 public class UserController {
-    @GetMapping// ! http://localhost:8080/users?page=<pageNo>&limit=<limitNo>
-    public String getUsers(@RequestParam(value = "page") int page,@RequestParam(value = "limit") int limit){
-        return "get users was called with page: "+page+" limit: "+limit;
+    @GetMapping// ! http://localhost:8080/users?page=<pageNo>&limit=<limitNo>&sort<sort>
+    // ! here sort a optinial parameter if you dont type ws return null for sort parameter
+    // ! req:http://localhost:8080/users?page=1&limit=50
+    // ! res:get users was called with page: 1 limit: 50sort: null
+    public String getUsers(
+            @RequestParam(value = "page",defaultValue = "1") int page
+            ,@RequestParam(value = "limit") int limit
+            ,@RequestParam(value = "sort",required = false) String sort){
+        return "get users was called with page: "+page+" limit: "+limit +" sort: "+sort;
     }
     @GetMapping(path="/{userId}")// ! http://localhost:8080/users/{userId}
-    public String getUser(@PathVariable String userId){
-        return "get user was called"+userId;
+    public UserRest getUser(@PathVariable String userId){
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail("sea@gmail.com");
+        returnValue.setFirstName("sea");
+        returnValue.setLastName("ase");
+
+        return returnValue;
     }
     @PostMapping// ! http://localhost:8080/users
     public String createUser(){
